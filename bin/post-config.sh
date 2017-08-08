276561260098b46c52bf10728ae1934d27130172
Sai Sindhur Malleni <smalleni@redhat.com>
2017-08-08 11:16:00 -0400

    Updates to post-config.sh and conf file

--- bin/post-config.sh
+++ bin/post-config.sh
@@ -14,12 +14,16 @@ ssh_priv_key=${quads["private_key_location"]}
 json_web_path=${quads["json_web_path"]}
 openstack_templates_dir=${quads["openstack_templates"]}
 openstack_templates_git=https://github.com/smalleni/automated-openstack-templates.git
+
+# trap to make sure 
 function finish {
-  rm $data_dir/postconfig/${env}-${owner}-${ticket}-${1}-start
+  rm ${data_dir}/postconfig/${env}-${owner}-${ticket}-${1}-start
 }
 trap finish EXIT
 
-apply_post_config() {
+# function that does the undercloud and overcloud deploy
+
+function apply_post_config() {
    setup_infrared
    pushd $infrared_dir/infrared
    source .venv/bin/activate
@@ -29,24 +33,30 @@ apply_post_config() {
    setup_inventory $1 $undercloud
    version=$($openstack_installer -c $1 -q | head -1)
    build=$($openstack_installer -c $1 -q | tail -1)
-   # TODO(sai): add clean_nodes option
-   IR_WORKSPACE=$1 ir tripleo-undercloud --version 11 --build GA --config-options DEFAULT.local_interface=em2
+   # defaults to em2 interface and enables cleaning of nodes by default
+   IR_WORKSPACE=$1 ir tripleo-undercloud --version 11 --build GA --config-options DEFAULT.local_interface=em2 --config-options DEFAULT.clean_nodes=true
    $openstack_installer -c $1 -i $2 -t $3 -uc $undercloud
    # wait for the openstack install script to write out appropriate
-   # instakcenv.json and for it to be available 
-   sleep 300
+   # instackenv.json and for it to be available 
+   sleep 120
    IR_WORKSPACE=$1 ir tripleo-undercloud --images-task rpm
    # introspect
    IR_WORKSPACE=$1 ir tripleo-overcloud --introspect yes --tag yes --version $version --instackenv-file $2 --deployment-files $3
+   # wait for nodes to be cleaned; need more comprehensive checks in the future
+   sleep 600
    # deploy
    IR_WORKSPACE=$1 ir tripleo-overcloud --introspect yes --tag yes --deploy yes --version $version --instackenv-file $2 --deployment-files $3
+   if [ "$?" -eq 0 ]; then
+       touch ${data_dir}/postconfig/${env}-${owner}-${ticket}-${1}-success
+   fi
 }
 
 
 
+# since our workspace name is just cloudname we will keep reusing names, so we
+# need to delete existing workspaces, export them and start fresh
 
-
-delete_workspace() {
+function delete_workspace() {
     workspace=$(ir wokspace list | grep -q $1)
     if [  "$workspace" != "" ]
     then
@@ -59,12 +69,15 @@ delete_workspace() {
 }
 
 
-create_workspace() {
+
+# Create workspace for infrared specific to cloudname
+function create_workspace() {
     infrared workspace create $1
 }
 
 
-setup_inventory() {
+# Pass cloudname as 1st argument and FQDN of undercloud as 2nd argument
+function setup_inventory() {
     cat <<EOF > $infrared_dir/infrared/.workspaces/$1/hosts
 $2 ansible_ssh_host=$2 ansible_ssh_user=root ansible_ssh_private_key_file=$ssh_priv_key
 localhost ansible_connection=local ansible_python_interpreter=python
@@ -74,13 +87,14 @@ $2
 
 [local]
 localhost
+EOF
+}
 
+# Installs and sets up infrared on the QUADS host if its hasn't been done
 
-# Pass "openstack" as first argument to this script
-
-setup_infrared() {
-    if [ ! -d $infrared_dir/infrared ]; then
-        git clone https://github.com/redhat-openstack/infrared.git $infrared_dir
+function setup_infrared() {
+    if [ ! -d $infrared_dir/infrared]; then
+        git clone https://github.com/redhat-openstack/infrared.git $infrared_dir/infrared
         pushd $infrared_dir/infrared
         virtualenv .venv && source .venv/bin/activate
         pip install --upgrade pip
@@ -91,18 +105,24 @@ setup_infrared() {
 
 }
 
+# Directory for all post-config related state (to track clouds with deployments
+# in progress vs undeployed clouds vs clouds that have had successful
+# deployments
 if [ ! -d ${data_dir}/postconfig ]; then
     mkdir ${data_dir}/postconfig
 fi
 
+
+# Pass "openstack" as 1st argument to this script
 
 for env in $($quads --summary --post-config $1 ; do
     owner=$($quads --ls-owner --cloud-only $env)
     ticket=$($quads --ls-ticket --cloud-only $env)
     if [ "$owner" != "nobody" -a "$owner" -a "$ticket" ]; then
         if [ -f $data_dir/release/${env}-${owner}-${ticket} ]; then
-            if [ -f $data_dir/postconfig/${env}-${owner}-${ticket}-${1}-start ] && [ ! -f {env}-${owner}-${ticket}-${1}-success ]; then
-                touch $data_dir/postconfig/${env}-${owner}-${ticket}-${1}-start
+                if [ ! -f
+                    ${data_dir}/postconfig/${env}-${owner}-${ticket}-${1}-start ] && [ ! -f ${data_dir}/postconfig/${env}-${owner}-${ticket}-${1}-success ]; then
+                touch ${data_dir}/postconfig/${env}-${owner}-${ticket}-${1}-start
                 # Clone OpenStack Templates
                 cloud_specific_templates=${openstack_templates_dir}/${env}-${owner}-${ticket}/automated-openstack-templates
                 if [ ! -d ${cloud_specific_templates} ]; then
